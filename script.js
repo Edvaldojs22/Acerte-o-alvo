081acf8e8f1ed2d625813d08bdfff0b3770d1425
Edvaldojs22 <124460629+Edvaldojs22@users.noreply.github.com>
2023-06-17 12:28:53 -0300

    final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var tela = document.querySelector("canvas");
 var pincel = tela.getContext("2d");
 const mensagem = document.querySelector(".parabens");
@@ -8,118 +10,236 @@ const velocidade = document.querySelector(".velocidade");
 const body = document.querySelector("body")
 
 
-
-var raio = 10
-
+var raio = 10;
 var xAleatorio;
 var yAleatorio;
+var jogoAtivo = true
 
 
-pincel.fillStyle = "#000000";
-pincel.fillRect(0, 0, 500, 300);
+if (window.matchMedia("(max-width: 600px)").matches) {
 
-function limpatela() {
-    pincel.clearRect(0, 0, 500, 300);
-}
+    tela.width = 340;
 
-function desenhaCirculo(x, y, raio, cor) {
-    pincel.fillStyle = cor;
-    pincel.beginPath();
-    pincel.arc(x, y, raio, 0, 2 * Math.PI);
-    pincel.fill();
+    pincel.fillStyle = "#000000";
+    pincel.fillRect(0, 0, 340, 300);
 
-}
+    function limpatela() {
+        pincel.clearRect(0, 0, 340, 300);
+    }
 
-function desenhaAlvo(x, y) {
+    function desenhaCirculo(x, y, raio, cor) {
+        pincel.fillStyle = cor;
+        pincel.beginPath();
+        pincel.arc(x, y, raio, 0, 2 * Math.PI);
+        pincel.fill();
 
-    desenhaCirculo(x, y, raio + 10, "red");
-    desenhaCirculo(x, y, raio + 5, "white")
-    desenhaCirculo(x, y, raio, "red");
-}
+    }
+
+    function desenhaAlvo(x, y) {
+
+        desenhaCirculo(x, y, raio + 5, "red");
+        desenhaCirculo(x, y, raio + 2, "white")
+        desenhaCirculo(x, y, raio, "red");
+    }
 
 
 
-var segundos = 500
+    var segundos = 500
 
-velocidade.innerHTML = segundos + "ms"
-
-function atualizaVelocidade() {
     velocidade.innerHTML = segundos + "ms"
-}
 
-aumentar.addEventListener("click", () => {
-    segundos = segundos + 100
-    atualizaVelocidade()
+    function atualizaVelocidade() {
+        velocidade.innerHTML = segundos + "ms"
+    }
 
-})
+    aumentar.addEventListener("click", () => {
+        segundos = segundos + 100
+        atualizaVelocidade()
 
-baixar.addEventListener("click", () => {
-    segundos = segundos - 100
-    atualizaVelocidade()
-})
+    })
 
-
-botaoStart.addEventListener("click", jogo)
-
-
-var intervalo = setInterval(repeticao, segundos)
-
-function jogo() {
-
-    pincel.fillStyle = "#0ad7e6";
-    pincel.fillRect(0, 0, 500, 300);
+    baixar.addEventListener("click", () => {
+        segundos = segundos - 100
+        atualizaVelocidade()
+    })
 
 
-    setTimeout(function () {
-
-        limpatela()
-
-        function desenhaAlvo(x, y) {
-
-            desenhaCirculo(x, y, raio + 10, "red");
-            desenhaCirculo(x, y, raio + 5, "white")
-            desenhaCirculo(x, y, raio, "red");
-        }
-
-        function sorteiaPosicao() {
-            return Math.floor(Math.random() * 500)
-
-        }
-
-        function repeticao() {
-
-            limpatela();
-            xAleatorio = sorteiaPosicao(500);
-            yAleatorio = sorteiaPosicao(300);
-            desenhaAlvo(xAleatorio, yAleatorio);
-        }
-
-        setInterval(repeticao, segundos);
-
-        tela.onclick = dispara;
-
-        function dispara(evento) {
-
-            var x = evento.pageX - tela.offsetLeft;
-            var y = evento.pageY - tela.offsetTop;
+    botaoStart.addEventListener("click", jogo)
 
 
-            if ((x < xAleatorio + raio)
-                && (x > xAleatorio - raio)
-                && (y > yAleatorio - raio)
-                && (y < yAleatorio + raio)) {
-                clearInterval(intervalo);
-                mensagem.style.display = "block";
-                body.classList.add('novo-body')
+    var intervalo = setInterval(repeticao, segundos)
+
+    function jogo() {
+
+        pincel.fillStyle = "#0ad7e6";
+        pincel.fillRect(0, 0, 350, 300);
+
+
+        setTimeout(function () {
+
+            limpatela()
+
+            function desenhaAlvo(x, y) {
+
+                desenhaCirculo(x, y, raio + 10, "red");
+                desenhaCirculo(x, y, raio + 5, "white")
+                desenhaCirculo(x, y, raio, "red");
+            }
+
+            function sorteiaPosicao() {
+                return Math.floor(Math.random() * 340)
 
             }
-            
-        }
-    }, 1000);
+
+            function repeticao() {
+
+                if (jogoAtivo) {
+                    limpatela();
+                    xAleatorio = sorteiaPosicao(340);
+                    yAleatorio = sorteiaPosicao(300);
+                    desenhaAlvo(xAleatorio, yAleatorio);
+                }
+
+            }
+
+            setInterval(repeticao, segundos);
+
+            tela.onclick = dispara;
+
+            function dispara(evento) {
+
+                var x = evento.pageX - tela.offsetLeft;
+                var y = evento.pageY - tela.offsetTop;
 
 
+                if ((x < xAleatorio + raio)
+                    && (x > xAleatorio - raio)
+                    && (y > yAleatorio - raio)
+                    && (y < yAleatorio + raio)) {
+                    clearInterval(intervalo);
+                    mensagem.style.display = "block";
+                    body.classList.add('novo-body');
+                    jogoAtivo = false;
+
+                }
+
+            }
+        }, 1000);
+    }
+    tela.onclick = dispara;
+
+}else {
+
+    tela.width = 500;
+    pincel.fillStyle = "#000000";
+    pincel.fillRect(0, 0, 500, 300);
+
+    function limpatela() {
+        pincel.clearRect(0, 0, 500, 300);
+    }
+
+    function desenhaCirculo(x, y, raio, cor) {
+        pincel.fillStyle = cor;
+        pincel.beginPath();
+        pincel.arc(x, y, raio, 0, 2 * Math.PI);
+        pincel.fill();
+
+    }
+
+    function desenhaAlvo(x, y) {
+
+        desenhaCirculo(x, y, raio + 10, "red");
+        desenhaCirculo(x, y, raio + 5, "white")
+        desenhaCirculo(x, y, raio, "red");
+    }
+
+
+
+    var segundos = 500
+
+    velocidade.innerHTML = segundos + "ms"
+
+    function atualizaVelocidade() {
+        velocidade.innerHTML = segundos + "ms"
+    }
+
+    aumentar.addEventListener("click", () => {
+        segundos = segundos + 100
+        atualizaVelocidade()
+
+    })
+
+    baixar.addEventListener("click", () => {
+        segundos = segundos - 100
+        atualizaVelocidade()
+    })
+
+
+    botaoStart.addEventListener("click", jogo)
+
+
+    var intervalo = setInterval(repeticao, segundos)
+
+    function jogo() {
+
+        pincel.fillStyle = "#0ad7e6";
+        pincel.fillRect(0, 0, 500, 300);
+
+
+        setTimeout(function () {
+
+            limpatela()
+
+            function desenhaAlvo(x, y) {
+
+                desenhaCirculo(x, y, raio + 10, "red");
+                desenhaCirculo(x, y, raio + 5, "white")
+                desenhaCirculo(x, y, raio, "red");
+            }
+
+            function sorteiaPosicao() {
+                return Math.floor(Math.random() * 500)
+
+            }
+
+            function repeticao() {
+
+                if (jogoAtivo) {
+                    limpatela();
+                    xAleatorio = sorteiaPosicao(500);
+                    yAleatorio = sorteiaPosicao(300);
+                    desenhaAlvo(xAleatorio, yAleatorio);
+                }
+
+            }
+
+            setInterval(repeticao, segundos);
+
+            tela.onclick = dispara;
+
+            function dispara(evento) {
+
+                var x = evento.pageX - tela.offsetLeft;
+                var y = evento.pageY - tela.offsetTop;
+
+
+                if ((x < xAleatorio + raio)
+                    && (x > xAleatorio - raio)
+                    && (y > yAleatorio - raio)
+                    && (y < yAleatorio + raio)) {
+                    clearInterval(intervalo);
+                    mensagem.style.display = "block";
+                    body.classList.add('novo-body');
+                    jogoAtivo = false;
+
+                }
+
+            }
+        }, 1000);
+    }
+    tela.onclick = dispara;
 
 }
-tela.onclick = dispara;
+
 
 
